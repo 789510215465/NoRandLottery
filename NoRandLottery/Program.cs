a8669625cfbef4f3565646879122a76353c51de5
m305-12\w401 <jjjj000123000@gmail.com>
2018-04-12 18:38:44 +0800

    first

---
+++ NoRandLottery/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoRandLottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> word = new List<string>();
+            word.Add("A");
+            word.Add("B");
+            word.Add("C");
+            word.Add("D");
+            word.Add("E");
+
+            Console.WriteLine( word[0] + word[1] + word[2] + word[3] + word[4]);
+
+            string tmp = word[1] ;
+            word[1] = word[4] ;
+            word[4] = tmp;
+
+            Console.WriteLine(word[0] + word[1] + word[2] + word[3] + word[4]);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
